5c9cfff4c3bef9c2304866e4e07e5e2775989d98
treborkaa <stephane.bran.photo@gmail.com>
2023-07-14 00:23:40 +0200

    Auto-generated commit [ci skip]

--- src/app/directives/img-loaded.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImgLoadedDirective } from './img-loaded.directive';
-
-describe('ImgLoadedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImgLoadedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/popup-loaded.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PopupLoadedDirective } from './popup-loaded.directive';
-
-describe('PopupLoadedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PopupLoadedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
